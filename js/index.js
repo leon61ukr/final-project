3d2dffccf1998d59c3ade3639249ac993099a510
Valeriy Yurchenko <leon61@ukr.net>
2018-03-14 13:56:21 +0000

    alfa2

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-new WOW().init();
 // swipes enable on iOS in Bootstrap4 carousel
 $('#carouselBikeIndicators').bcSwipe({ threshold: 30 });
-const productList = new ProductList('products.json', $('.products-container'), cart);
+
+new WOW().init();
